79f0bf0b70bf2e432a902c60b5c85869ddf44f8b
VivaMango <vivamangomail@gmail.com>
2019-05-07 09:29:44 -0700

    projects coming from db successfully after fixing git HEAD error from plane

--- client/src/pages/PortfolioPage/index.js
+++ client/src/pages/PortfolioPage/index.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import CustomCardGroup from "../../components/CustomCardGroup";
+import API from "../../utils/API";
 
 class PortfolioPage extends Component {
+
+    state = {
+        projects: []
+    };
+
+    componentDidMount() {
+        this.getProjects();
+    }
+
+    getProjects = _=> {
+        API.getProjects().then(res => {
+            console.log(res.data)
+            this.setState({
+                projects: res.data
+            })
+        })
+    };
  
     render () {
         return (
-            <CustomCardGroup 
-            image="PortfolioImage"
-            name="PortfolioName"
-            summary="PortfolioSummary"
-            buttonOneLink="repoUrl"
-            buttonOneText="repoText"
-            buttonTwoLink="deployURL"
-            buttonTwoText="deployText"/>
+            this.state.projects.map((project, index) => {
+                return (
+                    <CustomCardGroup
+                    key={index}
+                    name={project.name}
+                    image={project.image}
+                    summary={project.summary}
+                    repoLink={project.repoLink}
+                    deployLink={project.deployLink}
+                     />
+                )
+            })
         );
     };
 
